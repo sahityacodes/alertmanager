2126a818864aa8c4ea90be916a7657df2a1d2620
sahityacodes <49113898+sahityacodes@users.noreply.github.com>
2025-06-06 18:42:56 +0200

    Adding debug logs

--- notify/webhook/webhook.go
+++ notify/webhook/webhook.go
@@ -1,135 +1,110 @@
-// Copyright 2019 Prometheus Team
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package webhook
-
-import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"fmt"
-	"log/slog"
-	"net/http"
-	"os"
-	"strings"
-
-	commoncfg "github.com/prometheus/common/config"
-
-	"github.com/prometheus/alertmanager/config"
-	"github.com/prometheus/alertmanager/notify"
-	"github.com/prometheus/alertmanager/template"
-	"github.com/prometheus/alertmanager/types"
-)
-
-// Notifier implements a Notifier for generic webhooks.
-type Notifier struct {
-	conf    *config.WebhookConfig
-	tmpl    *template.Template
-	logger  *slog.Logger
-	client  *http.Client
-	retrier *notify.Retrier
-}
-
-// New returns a new Webhook.
-func New(conf *config.WebhookConfig, t *template.Template, l *slog.Logger, httpOpts ...commoncfg.HTTPClientOption) (*Notifier, error) {
-	client, err := commoncfg.NewClientFromConfig(*conf.HTTPConfig, "webhook", httpOpts...)
-	if err != nil {
-		return nil, err
-	}
-	return &Notifier{
-		conf:   conf,
-		tmpl:   t,
-		logger: l,
-		client: client,
-		// Webhooks are assumed to respond with 2xx response codes on a successful
-		// request and 5xx response codes are assumed to be recoverable.
-		retrier: &notify.Retrier{},
-	}, nil
-}
-
-// Message defines the JSON object send to webhook endpoints.
-type Message struct {
-	*template.Data
-
-	// The protocol version.
-	Version         string `json:"version"`
-	GroupKey        string `json:"groupKey"`
-	TruncatedAlerts uint64 `json:"truncatedAlerts"`
-}
-
-func truncateAlerts(maxAlerts uint64, alerts []*types.Alert) ([]*types.Alert, uint64) {
-	if maxAlerts != 0 && uint64(len(alerts)) > maxAlerts {
-		return alerts[:maxAlerts], uint64(len(alerts)) - maxAlerts
-	}
-
-	return alerts, 0
-}
-
 // Notify implements the Notifier interface.
 func (n *Notifier) Notify(ctx context.Context, alerts ...*types.Alert) (bool, error) {
-	alerts, numTruncated := truncateAlerts(n.conf.MaxAlerts, alerts)
-	data := notify.GetTemplateData(ctx, n.tmpl, alerts, n.logger)
+    // Add debug log for incoming alerts
+    n.logger.Debug("processing webhook notification", 
+        "alert_count", len(alerts),
+        "receiver", n.conf.Name,
+    )
 
-	groupKey, err := notify.ExtractGroupKey(ctx)
-	if err != nil {
-		// @tjhop: should we `return false, err` here as we do in most
-		// other Notify() implementations?
-		n.logger.Error("error extracting group key", "err", err)
-	}
+    alerts, numTruncated := truncateAlerts(n.conf.MaxAlerts, alerts)
+    data := notify.GetTemplateData(ctx, n.tmpl, alerts, n.logger)
 
-	// @tjhop: should we debug log the key here like most other Notify() implementations?
+    groupKey, err := notify.ExtractGroupKey(ctx)
+    if err != nil {
+        n.logger.Error("failed to extract group key", "err", err)
+    } else {
+        n.logger.Debug("extracted group key", "group_key", groupKey.String())
+    }
 
-	msg := &Message{
-		Version:         "4",
-		Data:            data,
-		GroupKey:        groupKey.String(),
-		TruncatedAlerts: numTruncated,
-	}
+    msg := &Message{
+        Version:         "4",
+        Data:            data,
+        GroupKey:        groupKey.String(),
+        TruncatedAlerts: numTruncated,
+    }
 
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(msg); err != nil {
-		return false, err
-	}
+    var buf bytes.Buffer
+    if err := json.NewEncoder(&buf).Encode(msg); err != nil {
+        n.logger.Error("failed to encode message", "err", err)
+        return false, err
+    }
 
-	var url string
-	if n.conf.URL != nil {
-		url = n.conf.URL.String()
-	} else {
-		content, err := os.ReadFile(n.conf.URLFile)
-		if err != nil {
-			return false, fmt.Errorf("read url_file: %w", err)
-		}
-		url = strings.TrimSpace(string(content))
-	}
+    // Log the complete payload being sent
+    n.logger.Debug("webhook request payload",
+        "payload", buf.String(),
+        "max_alerts", n.conf.MaxAlerts,
+        "truncated_alerts", numTruncated,
+    )
 
-	if n.conf.Timeout > 0 {
-		postCtx, cancel := context.WithTimeoutCause(ctx, n.conf.Timeout, fmt.Errorf("configured webhook timeout reached (%s)", n.conf.Timeout))
-		defer cancel()
-		ctx = postCtx
-	}
+    var url string
+    if n.conf.URL != nil {
+        url = n.conf.URL.String()
+    } else {
+        content, err := os.ReadFile(n.conf.URLFile)
+        if err != nil {
+            n.logger.Error("failed to read URL file", 
+                "path", n.conf.URLFile,
+                "err", err,
+            )
+            return false, fmt.Errorf("read url_file: %w", err)
+        }
+        url = strings.TrimSpace(string(content))
+    }
 
-	resp, err := notify.PostJSON(ctx, n.client, url, &buf)
-	if err != nil {
-		if ctx.Err() != nil {
-			err = fmt.Errorf("%w: %w", err, context.Cause(ctx))
-		}
-		return true, notify.RedactURL(err)
-	}
-	defer notify.Drain(resp)
+    n.logger.Debug("preparing webhook request",
+        "url", url,
+        "timeout", n.conf.Timeout,
+    )
 
-	shouldRetry, err := n.retrier.Check(resp.StatusCode, resp.Body)
-	if err != nil {
-		return shouldRetry, notify.NewErrorWithReason(notify.GetFailureReasonFromStatusCode(resp.StatusCode), err)
-	}
-	return shouldRetry, err
+    if n.conf.Timeout > 0 {
+        postCtx, cancel := context.WithTimeoutCause(ctx, n.conf.Timeout, fmt.Errorf("configured webhook timeout reached (%s)", n.conf.Timeout))
+        defer cancel()
+        ctx = postCtx
+    }
+
+    // Log right before sending
+    n.logger.Debug("sending webhook request",
+        "url", notify.RedactURL(url),
+        "content_length", buf.Len(),
+    )
+
+    resp, err := notify.PostJSON(ctx, n.client, url, &buf)
+    if err != nil {
+        n.logger.Error("webhook request failed",
+            "url", notify.RedactURL(url),
+            "err", err,
+            "context_error", ctx.Err(),
+            "context_cause", context.Cause(ctx),
+        )
+        if ctx.Err() != nil {
+            err = fmt.Errorf("%w: %w", err, context.Cause(ctx))
+        }
+        return true, notify.RedactURL(err)
+    }
+    defer notify.Drain(resp)
+
+    // Log response details
+    bodyBytes, _ := io.ReadAll(resp.Body)
+    n.logger.Debug("webhook response",
+        "status_code", resp.StatusCode,
+        "status", resp.Status,
+        "response_body", string(bodyBytes),
+        "url", notify.RedactURL(url),
+    )
+
+    shouldRetry, err := n.retrier.Check(resp.StatusCode, resp.Body)
+    if err != nil {
+        n.logger.Error("webhook notification failed",
+            "should_retry", shouldRetry,
+            "err", err,
+            "status_code", resp.StatusCode,
+            "failure_reason", notify.GetFailureReasonFromStatusCode(resp.StatusCode),
+        )
+        return shouldRetry, notify.NewErrorWithReason(notify.GetFailureReasonFromStatusCode(resp.StatusCode), err)
+    }
+
+    n.logger.Debug("webhook notification successful",
+        "status_code", resp.StatusCode,
+    )
+    return shouldRetry, err
 }
